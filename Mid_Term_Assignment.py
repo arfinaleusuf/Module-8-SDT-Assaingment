31fdd7f8c12f08e9f35bac22cd314933be9d78f6
arfinaleusuf <arfinaleusuf@gmail.com>
2026-04-30 12:21:59 +0600

    task 6

--- Mid_Term_Assignment.py
+++ Mid_Term_Assignment.py
@@ -20,7 +20,7 @@ class Student:
         return self.__is_enrolled
     
     @property
-    def get_student_id(self):
+    def student_id(self):
         return self.__student_id
 
     def enroll_student(self):
@@ -28,9 +28,19 @@ class Student:
             print('Error: Student is already enrolled')
         else:
             self.__is_enrolled = True
+            print(f'student {self.student_id} has been enrolled')
 
     def drop_student(self):
         if self.__is_enrolled:
             self.__is_enrolled = False
+            print(f'student {self.student_id} has been droped')
         else:
             print('Error: Student is not enrolled')
+
+    def view_student_info(self):
+        print(f'ID: {id}, Name: {self.__name}, Department: {self.__department}, Enrolled: {self.__is_enrolled}')
+
+
+s1 = Student(101,'Alice Smith', 'computer seience', True)
+s2 = Student(102,'Bob Jonson', 'Mathmatics', False)
+s3 = Student(103,'Charlie Lee', 'Physics', True)

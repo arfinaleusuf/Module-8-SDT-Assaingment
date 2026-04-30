d284602329f02ea5d3b9efc9a8c1a86deae2d50f
arfinaleusuf <arfinaleusuf@gmail.com>
2026-04-30 11:43:55 +0600

    enroll done

--- Mid_Term_Assignment.py
+++ Mid_Term_Assignment.py
@@ -9,7 +9,17 @@ class StudentDatabase:
 
 class Student:
     def __init__(self, student_id, name, department, is_enrolled = False):
-        self.student_id = student_id
-        self.name = name
-        self.department = department
-        self.is_enrolled = is_enrolled
+        self.__student_id = student_id
+        self.__name = name
+        self.__department = department
+        self.__is_enrolled = is_enrolled
+        StudentDatabase.add_student(self)
+
+    def is_enrolled(self):
+        return self.__is_enrolled
+
+    def enroll_student(self):
+        if self.__is_enrolled:
+            print('Error: Student Is Already Enrolled')
+        else:
+            self.__is_enrolled = True
